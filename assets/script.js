5b9f4efe8be762fac3090dc178c39b7776baa0eb
desktopbuddy <hanchondo21@gmail.com>
2023-07-17 17:46:45 -0700

    made intro element disappear when i click the start quiz button

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,52 @@
-var startQuiz = document.querySelector("#start-button")
-
-startQuiz.addEventListener("click", )
+var startBtn = document.querySelector("#start-button");
+var questionEl = document.querySelector("#question");
+var currentQuestionIndex = 0;
 
 //Make array that holds question objects
 var questions = [
     {
       title: 'Commonly used data types do not include ______',
       choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: '<script>',
+      answer: 'booleans',
     },
     {
       title: 'Arrays in JavaScript can be used to store ______',
       choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-      answer: 'The <body> section',
+      answer: 'booleans',
     },
     {
       title: 'String values must be enclosed within _____ when being assigned to variables.',
       choices: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-      answer: 'myFunction()',
+      answer: 'quotes',
     },
     {
       title: 'A very useful tool used during developement and debugging for printing conent debugger is:',
       choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
-      answer: '//This is a comment',
+      answer: 'console.log',
     },
     {
-      title:
-        'The condition in an if/else statement is enclosed with ____',
+      title:'The condition in an if/else statement is enclosed with ____',
       choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-      answer: 'All of the above.',
+      answer: 'curly brackets',
     },
   ];
 
 //When start quiz is pressed delete intro and fill question section with first question and activate timer
+startBtn.addEventListener("click", startQuiz)
+
+function startQuiz() {
+    var introEl = document.querySelector("#intro");
+
+    introEl.setAttribute('class', 'none');
+
+    questionEl.removeAttribute('class');
+    
+    getQuestions()
+}
+
+function getQuestions() {
+  var currentQuestion
+}
 
 // if i click right answer move on to next question 
 // if i click wrong answer then take off 10 seconds and move on to next question
